12603ce5044ee5bbd5c96a995e250c357df1f9ef
Smith Scarborough <smithscarborough@gmail.com>
2021-02-27 16:48:38 -0600

    adds styling consistency in comment/instructions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,10 @@
-/* Write a program that prints the numbers from 1 to 100. 
-But for multiples of three print Fizz instead of the number 
-and for the multiples of five print Buzz. For numbers 
-which are multiples of both three and five print FizzBuzz. */
+/* Write a program that accepts a number from the user,
+and prints from 1 to that number.  But for multiples of 3, 
+print 'Fizz' instead of the number; for multiples of 5, 
+print 'Buzz' instead of the number; and for multiples of
+both 3 and 5, print 'FizzBuzz' instead of the number. */
 
-number = prompt('Please enter a number to use for fizzBuzz: ');
+number = prompt('Please enter a number to use for FizzBuzz: ');
 
 for (var i = 1; i <= number; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
