0d2f1d2dfdffd033e2270dc5c0d9c991560a4115
Smith Scarborough <smithscarborough@gmail.com>
2021-02-27 19:13:17 -0600

    adds minor changes to the comments/instructions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,8 @@
 /* Write a program that accepts a number from the user,
-and prints from 1 to that number.  But for multiples of 3, 
-print 'Fizz' instead of the number; for multiples of 5, 
-print 'Buzz' instead of the number; and for multiples of
-both 3 and 5, print 'FizzBuzz' instead of the number. */
+and prints from 1 up to that number, but for multiples of 3, 
+prints 'Fizz' instead of the number; for multiples of 5, 
+prints 'Buzz' instead of the number; and for multiples of
+both 3 and 5, prints 'FizzBuzz' instead of the number. */
 
 number = prompt('Please enter a number to use for FizzBuzz: ');
 
